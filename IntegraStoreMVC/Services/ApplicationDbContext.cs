ddeec249e7962f8032c073808d29fccf15e7de28
tyronegnz <64057457+tyronegnz@users.noreply.github.com>
2024-06-29 13:00:08 -0600

    Add project files.

---
+++ IntegraStoreMVC/Services/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using IntegraStoreMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IntegraStoreMVC.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options): base(options)
+        {
+            
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
